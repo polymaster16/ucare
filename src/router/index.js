c6b871db03a8a86ea747e7376862d97fbf0233f3
polymaster16 <uili.kam8@gmail.com>
2023-05-22 14:35:23 +0100

    modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import SignIn from '../views/sign-in.vue'
 import Start from '../views/start.vue'
 
 const router = createRouter({
+  
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
